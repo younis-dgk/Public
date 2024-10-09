271739c1b96e2b3e9258ce25bf1ae9d1f4981594
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 16:05:11 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,6 +1,6 @@
 import os
 os.system("clear")
-x='\033[0;97mThis has been Temporary Closed\nPlease Wait For Next Update'
+x='\033[0;97mThis Tool has been Temporary Closed\nPlease Wait For Next Update'
 y='Authorized by : Muhammad Younis'
 z='For More About Author : https://bio.link/younis_dgk'
 print(x)
